64ecdb2721c01165bd075e62f1caf72c42072f9b
Jasper Zhao <jasper@Jaspers-MBP.fios-router.home>
2019-11-06 14:06:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,41 @@
+/*
+
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.icon:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
+
+
+*/
---
+++ app/src/main/java/model/CurrentCondition.java
@@ -0,0 +1,86 @@
+//package model;
+//
+//public class CurrentCondition {
+//
+//    private int weatherId;
+//    private int condition;
+//    private String description;
+//    private String icon;
+//    private float pressure;
+//    private float humidity;
+//    private float maxTemp;
+//    private float minTemp;
+//    private double temperature;
+//
+//    public int getWeatherId() {
+//        return weatherId;
+//    }
+//
+//    public void setWeatherId(int weatherId) {
+//        this.weatherId = weatherId;
+//    }
+//
+//    public int getCondition() {
+//        return condition;
+//    }
+//
+//    public void setCondition(int condition) {
+//        this.condition = condition;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDecription(String description) {
+//        this.description = description;
+//    }
+//
+//    public String getIcon() {
+//        return icon;
+//    }
+//
+//    public void setIcon(String icon) {
+//        this.icon = icon;
+//    }
+//
+//    public float getPressure() {
+//        return pressure;
+//    }
+//
+//    public void setPressure(float pressure) {
+//        this.pressure = pressure;
+//    }
+//
+//    public float getHumidity() {
+//        return humidity;
+//    }
+//
+//    public void setHumidity(float humidity) {
+//        this.humidity = humidity;
+//    }
+//
+//    public float getMaxTemp() {
+//        return maxTemp;
+//    }
+//
+//    public void setMaxTemp(float maxTemp) {
+//        this.maxTemp = maxTemp;
+//    }
+//
+//    public float getMinTemp() {
+//        return minTemp;
+//    }
+//
+//    public void setMinTemp(float minTemp) {
+//        this.minTemp = minTemp;
+//    }
+//
+//    public double getTemperature() {
+//        return temperature;
+//    }
+//
+//    public void setTemperature(double temperature) {
+//        this.temperature = temperature;
+//    }
+//}
